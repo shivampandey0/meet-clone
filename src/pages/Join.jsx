d69e9478fb0a9a3a3f9a108ca5905d620b744bdd
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-06-15 19:03:15 +0530

    [style] [refactor] guest Login added

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -26,32 +26,51 @@ export const Join = () => {
     }
   };
 
+  const handleGuestLogin = () => {
+    try {
+      hmsActions.join({
+        userName: inputValues.name || 'Guest',
+        authToken: process.env.REACT_APP_GUEST_AUTH,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className='rounded-xl border bg-opacity-50 backdrop-blur-2xl bg-slate-500 shadow-xl w-96 xl:w-3/12'>
-      <form
-        className='flex flex-col justify-between gap-4 w-full p-4'
-        onSubmit={handleSubmit}
-      >
-        <h2 className='text-white text-2xl font-bold'>Join Meeting</h2>
+    <div className='w-80 xl:w-3/12 h-full flex flex-col justify-center'>
+      <div className='rounded-xl border bg-opacity-50 backdrop-blur-2xl bg-slate-500 shadow-xl h-fit '>
+        <form
+          className='flex flex-col justify-between gap-4 w-full p-4'
+          onSubmit={handleSubmit}
+        >
+          <h2 className='text-white text-2xl font-bold'>Join Meeting</h2>
 
-        <Input
-          value={inputValues?.name}
-          onChange={handleInputChange}
-          id='name'
-          type='text'
-          placeholder='Your name'
-        />
+          <Input
+            value={inputValues?.name}
+            onChange={handleInputChange}
+            id='name'
+            type='text'
+            placeholder='Your name'
+          />
 
-        <Input
-          value={inputValues?.token}
-          onChange={handleInputChange}
-          id='token'
-          type='text'
-          placeholder='Auth token'
-        />
+          <Input
+            value={inputValues?.token}
+            onChange={handleInputChange}
+            id='token'
+            type='text'
+            placeholder='Auth token'
+          />
 
-        <Button type='submit' title='Join' />
-      </form>
+          <Button type='submit' title='Join' />
+          <div className='flex justify-center items-center'>
+            <div className='flex-grow border-t border-white-400'></div>
+            <h3 className='text-center text-white px-1'>Or</h3>
+            <div className='flex-grow border-t border-white-400'></div>
+          </div>
+          <Button onClick={handleGuestLogin} title='Join as Guest' />
+        </form>
+      </div>
     </div>
   );
 };
