8a9fc4dc92def6a334e9232f1119b07a5205e7aa
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-06-15 19:05:12 +0530

    [feat] Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className='py-4'>
+      <h1 className='text-white text-2xl font-bold'>Meet Clone</h1>
+    </header>
+  );
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { Input } from './Input';
 export { Button } from './Button';
+export { Peer } from './Peer';
+export { Footer } from './Footer';
+export { Header } from './Header';
