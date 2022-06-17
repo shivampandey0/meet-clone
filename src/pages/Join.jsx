4bb642b8a103e706667212bd08dac4abde548d11
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-06-17 18:20:37 +0530

    [refactor] auth token generation

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,10 +1,11 @@
 import { useHMSActions } from '@100mslive/react-sdk';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Input } from '../components';
 
 export const Join = () => {
   const hmsActions = useHMSActions();
   const [inputValues, setInputValues] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (e) => {
     setInputValues((prevValues) => ({
@@ -26,11 +27,21 @@ export const Join = () => {
     }
   };
 
+  useEffect(() => {
+    setLoading(true);
+    fetch('/api/app_token')
+      .then((res) => res.json())
+      .then((data) => {
+        setInputValues((prev) => ({ ...prev, ...data }));
+      })
+      .finally(setLoading(false));
+  }, []);
+
   const handleGuestLogin = () => {
     try {
       hmsActions.join({
         userName: inputValues.name || 'Guest',
-        authToken: process.env.REACT_APP_GUEST_AUTH,
+        authToken: inputValues.token || process.env.REACT_APP_GUEST_AUTH,
       });
     } catch (error) {
       console.log(error);
@@ -68,7 +79,11 @@ export const Join = () => {
             <h3 className='text-center text-white px-1'>Or</h3>
             <div className='flex-grow border-t border-white-400'></div>
           </div>
-          <Button onClick={handleGuestLogin} title='Join as Guest' />
+          <Button
+            disabled={loading}
+            onClick={handleGuestLogin}
+            title='Join as Guest'
+          />
         </form>
       </div>
     </div>
