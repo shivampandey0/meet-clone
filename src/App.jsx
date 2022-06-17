6eb625c4da419316902fd397be966bd7ec49aa1c
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-06-17 18:20:19 +0530

    [refactor] management token fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {
   useHMSActions,
   useHMSStore,
 } from '@100mslive/react-sdk';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Conference } from './pages/Conference';
 import { Footer } from './components/Footer';
 import { Header } from './components';
@@ -12,16 +12,33 @@ import { Header } from './components';
 const App = () => {
   const hmsActions = useHMSActions();
   const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const [sessionInfo, setSessionInfo] = useState();
 
   useEffect(() => {
     window.onunload = () => {
       if (isConnected) hmsActions.leave();
     };
+    if (isConnected) {
+      fetch('/api/session')
+        .then((res) => res.json())
+        .then((data) =>
+          fetch(
+            'https://api.100ms.live/v2/sessions?room_id=629dc724b873787aa270296b&active=true',
+            {
+              headers: {
+                Authorization: `Bearer ${data.token}`,
+              },
+            }
+          )
+            .then((res) => res.json())
+            .then((data) => setSessionInfo(data))
+        );
+    }
   }, [hmsActions, isConnected]);
 
   return (
     <main className='w-screen h-screen flex flex-col p-4 items-center bg-gradient-to-b px-2 from-gray-900 to-sky-900'>
-      <Header />
+      <Header sessionInfo={sessionInfo} />
       {isConnected ? (
         <>
           <Conference />
